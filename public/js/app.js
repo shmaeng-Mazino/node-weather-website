396e72a961fd5ddd4742a6e32c8d29783e94b7bc
MaengSH <50561739+shmaeng@users.noreply.github.com>
2021-12-13 11:56:04 +0900

    remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -2,11 +2,11 @@ console.log('Client side javascript');
 
 // http://puzzle.mead.io/puzzle
 
-fetch('http://puzzle.mead.io/puzzle').then(response => {
-	response.json().then(data => {
-		console.log(data);
-	});
-});
+// fetch('http://puzzle.mead.io/puzzle').then(response => {
+// 	response.json().then(data => {
+// 		console.log(data);
+// 	});
+// });
 
 // http://localhost:3000/weather?address=boston
 
